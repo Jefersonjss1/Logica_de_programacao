0975086ed25e7ab2d57e171fed1be872c07b8593
Jefersonjss1 <jajinho11@gmail.com>
2023-07-25 17:27:25 -0300

    nova alteração

---
+++ 04 - Estruturas_programação/11 - switch/script.js
@@ -0,0 +1,120 @@
+/*let diaDaSemana = parseInt(prompt("Digite o dia da semana"));
+
+switch(diaDaSemana) {
+    case 1:
+        console.log("Segunda-Feira");
+        break;
+
+    case 2:
+        console.log("Terça-Feira");
+        break;
+
+    case 3:
+        console.log("Quarta-Feira");
+        break;
+
+    case 4:
+        console.log("Quinta-Feira");
+        break; 
+
+    case 5:
+        console.log("Sexta-Feira");
+        break;
+
+        default:
+            console.log("Fim de semana");
+}*/
+
+
+/*let nomeDaFruta = prompt();
+
+switch(nomeDaFruta) {
+    case "limão":
+    case "uva":
+    case "laranja":
+        console.log("Essa fruta é uma fruta cítrica");
+        break;
+    case "abacate":    
+    case "manga":
+        console.log("Essa fruta é uma fruta tropicai");
+        break;
+        default:
+            console.log("Não sei qual é o tipo desta fruta")    
+
+}*/
+
+/*let primeiroNumero = parseFloat(prompt("Digite um número"));
+let segundoNumero = parseFloat(prompt("Digite um número"));
+
+let calculadora = prompt("Digite uma operação\n [+] Soma\n [-] Subtração\n [/] Divisão");
+
+
+switch(calculadora) {
+    case "+":
+        console.log(`A soma do ${primeiroNumero} + ${segundoNumero} = ${primeiroNumero + segundoNumero}`)
+        break;
+    case "-":
+        console.log(`A subtração do ${primeiroNumero} - ${segundoNumero} = ${primeiroNumero - segundoNumero}`)
+        break;
+    case "/":
+        console.log(`A divisão do ${primeiroNumero} / ${segundoNumero} = ${primeiroNumero / segundoNumero}`)
+        break;
+        default:
+            console.log("Não existe a operação selecionada")        
+}*/
+
+/**
+ * Escreva um programa em JavaScript que solicite ao usuário que insira um número e verifique se ele é positivo, negativo ou zero. Se o número for maior que zero, o programa deve exibir "O número é positivo". Se o número for menor que zero, o programa deve exibir "O número é negativo". Se o número for igual a zero, o programa deve exibir "O número é zero". Se a entrada do usuário não for um número válido, o programa deve exibir "Entrada inválida".
+ 
+Para implementar a verificação, utilize um switch statement. O programa deve seguir a seguinte estrutura:
+ 
+Solicitar ao usuário que insira um número.
+Verificar se o número é positivo, negativo ou zero utilizando um switch statement.
+Exibir a mensagem correspondente ao tipo de número.
+Se a entrada do usuário não for um número válido, exibir "Entrada inválida".
+ */
+
+/*let numero = parseInt(prompt("Digite um número"));
+switch(true) {
+    case (numero > 0):
+        console.log("O número é positivo");
+        break;
+    case (numero < 0):
+        console.log("O número é negativo");
+        break;
+    case (numero == 0):
+        console.log("Zero");
+        break;
+        default:
+            console.log("Seu número não está nos padrões")        
+}*/
+
+/*Escreva um programa em JavaScript que solicite ao usuário que insira um número correspondente ao dia da semana (1-7) e exiba o nome do dia da semana correspondente à entrada do usuário. Se a entrada do usuário não for um número válido, o programa deve exibir "Entrada inválida".*/
+
+/*let numero = parseInt(prompt("Digite o número da semana"));
+
+switch(numero) {
+    case 1:
+        console.log("Domingo");
+        break;
+    case 2:
+        console.log("Segunda-Feira");
+        break;
+    case 3:
+        console.log("Terça-Feira");
+        break;
+    case 4:
+        console.log("Quarta-Feira");
+        break;
+    case 5:
+        console.log("Quinta-Feira");
+        break;
+    case 6:
+        console.log("Sexta-Feira");
+        break;
+    case 7:
+        console.log("Sabádo");
+        break;
+        default:
+            console.log("Entrada inválida")
+}*/
